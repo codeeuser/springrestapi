21964d3e854e57839013f8fd5d0f81d80b8ee61c
codeeuser <codeeuser@gmail.com>
2021-04-21 23:16:51 +0800

    post method

--- src/main/java/com/wheref/springrestapi/controller/WelcomeController.java
+++ src/main/java/com/wheref/springrestapi/controller/WelcomeController.java
@@ -6,6 +6,7 @@ import java.util.Random;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,9 +28,21 @@ public class WelcomeController {
         map.put("Jan", randomNum(10.0, 200.0));
         map.put("Feb", randomNum(10.0, 200.0));
         map.put("Mar", randomNum(10.0, 200.0));
-        map.put("Apr", randomNum(10.0, 200.0));
+        map.put("Apr", randomNum(-100.0, 200.0));
         map.put("May", randomNum(10.0, 200.0));
         map.put("Jun", randomNum(10.0, 200.0));
+		return map;
+	}
+
+    @PostMapping("/postChartData")
+	public Map<String, Double> postChartData() {
+        Map<String, Double> map = new LinkedHashMap<String, Double>();
+        map.put("Jul", randomNum(10.0, 200.0));
+        map.put("Aug", randomNum(10.0, 200.0));
+        map.put("Sep", randomNum(10.0, 200.0));
+        map.put("Oct", randomNum(-100.0, 200.0));
+        map.put("Nov", randomNum(10.0, 200.0));
+        map.put("Dec", randomNum(10.0, 200.0));
 		return map;
 	}
 
